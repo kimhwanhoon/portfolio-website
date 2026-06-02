44d74e3da8c388b4e261f31a841d471c1e762d41
Hwanhoon Kim <contact@hwanhoon.kim>
2026-06-03 00:42:31 +0200

    Added vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter, Montserrat } from "next/font/google";
 import { getLocale, getTranslations } from "next-intl/server";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { SITE_AUTHOR, SITE_URL, SITE_VERIFICATION } from "@/lib/site-config";
 import { cn } from "@/lib/utils";
@@ -77,6 +78,7 @@ export default async function RootLayout({
             </a>
             {children}
             <Analytics />
+            <SpeedInsights />
           </ThemeProvider>
         </body>
       </html>
